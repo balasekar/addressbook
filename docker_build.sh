647ad3224751cdf3b91218dfdcba6c40b0f2bc39
balasekar <baalasekar@gmail.com>
2017-05-01 14:56:00 +0100

    docker build fix

--- docker_build.sh
+++ docker_build.sh
@@ -4,19 +4,17 @@
 PROJECT_NAME=addressbook
 
 # Get some information from git if not already set
-GIT_COMMIT=${GIT_COMMIT:-`git rev-parse HEAD`}
-GIT_BRANCH=${GIT_BRANCH:-`git rev-parse --abbrev-ref HEAD`}
+GIT_BRANCH=${GIT_BRANCH}
 GIT_BRANCH=`echo $GIT_BRANCH | sed 's|/|_|g'`
 
 # Get Docker to build our container
 echo "Building docker container ${PROJECT_NAME}"
 echo "  Branch: ${GIT_BRANCH}"
-echo "  Commit: ${GIT_COMMIT}"
 
 # copy WAR file into the current directory
 cp /var/lib/jenkins/workspace/addressbook_build/target/addressbook-2.0.war .
 
-docker build --build-arg "GIT_COMMIT=${GIT_COMMIT}" --build-arg "GIT_BRANCH=${GIT_BRANCH}" -t "balasekar/${PROJECT_NAME}:${GIT_BRANCH}" .
+docker build  --build-arg "GIT_BRANCH=${GIT_BRANCH}" -t "balasekar/${PROJECT_NAME}:${GIT_BRANCH}" .
 
 # Push the repository to our private Docker registry
 docker push "balasekar/${PROJECT_NAME}:${GIT_BRANCH}"
